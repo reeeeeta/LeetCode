f4d7b54426e8bb8e6d1dc2e61f4a2e196df1e10d
314581002吳靜雯 <139305744+reeeeeta@users.noreply.github.com>
2026-03-19 14:26:57 +0800

    Create 493_Reverse_Pairs.py

---
+++ Week2/493_Reverse_Pairs.py
@@ -0,0 +1,34 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        self.count = 0
+        def merge(left_arr, right_arr):
+            i, j = 0, 0
+            while i < len(left_arr) and j < len(right_arr):
+                if left_arr[i] > 2 * right_arr[j]:
+                    self.count += len(left_arr) - i
+                    j += 1
+                else:
+                    i += 1
+            
+            l, r = 0, 0
+            res = []
+            while l < len(left_arr) and r < len(right_arr):
+                if left_arr[l] < right_arr[r]:
+                    res.append(left_arr[l])
+                    l += 1
+                else:
+                    res.append(right_arr[r])
+                    r += 1
+            return res + left_arr[l:] + right_arr[r:] # 將剩下的元素接到後方
+
+        def devide(arr):
+            if len(arr) <=1: return arr
+
+            mid = len(arr)//2
+            left_arr = devide(arr[:mid])
+            right_arr = devide(arr[mid:])
+
+            return merge(left_arr, right_arr)
+
+        nums = devide(nums)
+        return self.count

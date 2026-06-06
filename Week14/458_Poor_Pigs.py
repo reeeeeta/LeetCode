1817f2822b5e2bc0e5bcefdab6118889bbb2409a
314581002吳靜雯 <139305744+reeeeeta@users.noreply.github.com>
2026-06-06 18:17:08 +0800

    Create 458_Poor_Pigs.py

---
+++ Week14/458_Poor_Pigs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        rounds = minutesToTest // minutesToDie
+        states = rounds + 1
+
+        pigs = 0
+        while (states ** pigs) < buckets:
+            pigs += 1
+
+        return pigs
